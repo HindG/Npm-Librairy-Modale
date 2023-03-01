b3e36dfdea7e97f236181e6576f991088de9f973
Hind <guessoushind@gmail.com>
2023-03-01 22:45:24 +0100

    Add modale

---
+++ src/lib/Modale.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./modale.css";
+
+const Modale = () => (
+<div id="confirmation" class="modal">Employee Created!</div>
+);
+
+export default Modale;
---
+++ src/lib/lib.js
@@ -0,0 +1,2 @@
+import Modale from "./TextInput";
+export { Modale };
